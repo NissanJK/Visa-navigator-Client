f8bc3491618a06860e8342fc77bbf3207b17a6bb
NissanJK <jawadul.karim78@gmail.com>
2024-12-08 19:52:01 +0600

    helmet added to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,20 @@ import Banner from "../components/Banner";
 import ExtraSectionOne from "../components/ExtraSectionOne";
 import ExtraSectionTwo from "../components/ExtraSectionTwo";
 import LatestVisas from "../components/LatestVisas";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
-  return (
-    <div>
-      <Banner></Banner>
-      <LatestVisas></LatestVisas>
-      <ExtraSectionOne></ExtraSectionOne>
-      <ExtraSectionTwo></ExtraSectionTwo>
-    </div>
-  );
+    return (
+        <div>
+            <Helmet>
+                <title>Visa Navigator | Home</title>
+            </Helmet>
+            <Banner></Banner>
+            <LatestVisas></LatestVisas>
+            <ExtraSectionOne></ExtraSectionOne>
+            <ExtraSectionTwo></ExtraSectionTwo>
+        </div>
+    );
 };
 
 export default Home;
